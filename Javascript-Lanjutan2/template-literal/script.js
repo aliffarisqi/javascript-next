d70e2016d4e7410956cdb9700c8a6835ee365523
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-09 19:08:39 +0700

    example concept of template-literal

--- Javascript-Lanjutan2/template-literal/script.js
+++ Javascript-Lanjutan2/template-literal/script.js
@@ -1,1 +1,11 @@
 console.log("ok");
+
+// template literal
+const nama = "bayu alif";
+const umur = "23";
+const bulan = "5";
+console.log(`hallo nama saya ${nama} umur saya ${umur} tahun`);
+
+//menyimpan ternari operator
+const x = 10;
+console.log(`${x % 2 == 0 ? "genap" : "ganjil"}`);
