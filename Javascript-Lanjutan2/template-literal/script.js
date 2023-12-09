e27678ca916d2c905568ae02649617007e35aa3b
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-09 19:52:25 +0700

    example concept of template-literal 2

--- Javascript-Lanjutan2/template-literal/script.js
+++ Javascript-Lanjutan2/template-literal/script.js
@@ -3,9 +3,60 @@ console.log("ok");
 // template literal
 const nama = "bayu alif";
 const umur = "23";
-const bulan = "5";
+const nrp = "8279812";
 console.log(`hallo nama saya ${nama} umur saya ${umur} tahun`);
 
 //menyimpan ternari operator
 const x = 10;
 console.log(`${x % 2 == 0 ? "genap" : "ganjil"}`);
+
+const mhs = {
+  nama: "bayu alif",
+  umur: "23",
+  nrp: "8279812",
+};
+const el = `<div class="mhs">
+<h2 class="">${mhs.nama}</h2>
+<span class="nrp">${mhs.nrp}</span>
+</div>`;
+
+// document.body.innerHTML = el;
+
+const mhsArray = [
+  {
+    nama: "alif",
+    umur: "22",
+    nrp: "8279812",
+    matkul: ["kalulus", "alpro"],
+  },
+  {
+    nama: "alif farisqi",
+    umur: "23",
+    nrp: "23423",
+    status: "lulus",
+    matkul: ["statistik", "alpro", "databases"],
+  },
+];
+
+function cetakMatkul(mataKuliah) {
+  return `
+  <ol class="">
+    ${mataKuliah.map((mk) => `<li class="">${mk}</li>`).join("")}
+  </ol>
+  `;
+}
+const elLoop = `
+<div class="nama">
+${mhsArray
+  .map(
+    (m) => `<ul class="">
+  <h2 class="">${m.nama} ${m.status ? `(${m.status})` : ""}</h2>
+  <h4 class="">Nrp : ${m.nrp}</h4>
+  <h4 class="">Mata kuliah</h4>
+  ${cetakMatkul(m.matkul)}
+</ul>`
+  )
+  .join("")}
+</div>
+`;
+document.body.innerHTML = elLoop;
