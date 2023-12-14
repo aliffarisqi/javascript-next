889b04ac8df7e5b36794d7e4f5ef9bf2fd66334d
Bayu Alif Farisqi <jakartabanyu@gmail.com>
2023-12-14 08:31:00 +0700

    refactoring fetch using async await

---
+++ Javascript-Lanjutan2/asynchronous/fetch-refactor/script.js
@@ -0,0 +1,79 @@
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", async function () {
+  const inputKeyword = document.querySelector(".input-keyword");
+  const movie = await getMovies(inputKeyword.value);
+  updateUI(movie);
+});
+
+//event Binding
+document.addEventListener("click", async function (e) {
+  if (e.target.classList.contains("modal-detail-button")) {
+    const imdbid = e.target.dataset.imdbid;
+    const moviewDetail = await getMovieDetail(imdbid);
+    updateUIDetail(moviewDetail);
+  }
+});
+function getMovieDetail(imdbid) {
+  return fetch("http://www.omdbapi.com/?apikey=4e9a0e25&i=" + imdbid)
+    .then((response) => response.json())
+    .then((m) => m);
+}
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector(".modal-body");
+  modalBody.innerHTML = movieDetail;
+}
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=4e9a0e25&s=" + keyword)
+    .then((response) => response.json())
+    .then((response) => response.Search);
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
+
+function showCards(m) {
+  return `<div class="col-md-3 my-3">
+            <div class="card">
+              <img src="${m.Poster}" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">${m.Title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal"
+                data-target="#moviewDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+              </div>
+            </div>
+          </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+            <div class="row">
+              <div class="col-md-3">
+                <img src="${m.Poster}" class="img-fluid" alt="" />
+              </div>
+              <div class="col-md">
+                <ul class="list-group">
+                  <li class="list-group-item"><h4>${m.Title} (${m.year})</h4></li>
+                  <li class="list-group-item">
+                    Director : <strong>${m.Director}</strong>
+                  </li>
+                  <li class="list-group-item">
+                    Actors : <strong>${m.Actors}</strong>
+                  </li>
+                  <li class="list-group-item">
+                    Writer : <strong>${m.Writer}</strong>
+                  </li>
+                  <li class="list-group-item">
+                    Plot : ${m.Plot}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>`;
+}
